17724b70a445d2e93b101173afc7aadb2b2973af
Ruthger Dijt <ruthgerdijt@gmail.com>
2021-09-27 11:15:02 +0200

    backend/*: kick start eiffelvis + _core / _http / _stream

---
+++ backend/crates/eiffelvis_core/src/types.rs
@@ -0,0 +1,43 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub struct BaseData {}
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub struct BaseMeta {
+    pub id: Uuid,
+    #[serde(rename = "type")]
+    pub event_type: String,
+    pub version: String,
+    pub time: u64,
+}
+
+#[derive(Default, Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize)]
+pub struct BaseLink {
+    #[serde(rename = "type")]
+    pub link_type: String,
+    pub target: Uuid,
+}
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub struct BaseEvent {
+    pub meta: BaseMeta,
+    pub data: BaseData,
+    pub links: Vec<BaseLink>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LeanEvent {
+    pub id: Uuid,
+    pub edges: Vec<Uuid>,
+}
+
+impl From<&BaseEvent> for LeanEvent {
+    fn from(ev: &BaseEvent) -> Self {
+        Self {
+            id: ev.meta.id,
+            edges: ev.links.iter().map(|link| link.target).collect(),
+        }
+    }
+}
---
+++ backend/crates/eiffelvis_stream/src/ampq.rs
@@ -0,0 +1,72 @@
+use std::{borrow::Cow, str::FromStr};
+
+use futures::StreamExt;
+
+use lapin::{tcp::TLSConfig, uri::AMQPUri, Connection, ConnectionProperties, Consumer};
+use tokio_amqp::LapinTokioExt;
+
+pub struct AmpqStream {
+    addr: Cow<'static, str>,
+    queue: Cow<'static, str>,
+    consumer_tag: Cow<'static, str>,
+    consumer: Option<Consumer>,
+}
+
+impl AmpqStream {
+    pub async fn new(
+        addr: Cow<'static, str>,
+        queue: Cow<'static, str>,
+        consumer_tag: Cow<'static, str>,
+    ) -> Option<Self> {
+        Some(Self {
+            consumer: Some(make_ampq_consumer(&addr, &queue, &consumer_tag).await?),
+            addr,
+            queue,
+            consumer_tag,
+        })
+    }
+
+    /// Yields the next message.
+    /// Returns None on failure, reconnects on the next call; thus it's recommended to wait a bit before trying again.
+    pub async fn next(&mut self) -> Option<Vec<u8>> {
+        if self.consumer.is_none() {
+            self.consumer = make_ampq_consumer(&self.addr, &self.queue, &self.consumer_tag).await;
+        }
+        if let Some(consumer) = &mut self.consumer {
+            if let Some(Ok((_, delivery))) = consumer.next().await {
+                Some(delivery.data)
+            } else {
+                None
+            }
+        } else {
+            self.consumer = None;
+            None
+        }
+    }
+}
+
+async fn make_ampq_consumer(addr: &str, queue: &str, consumer_tag: &str) -> Option<Consumer> {
+    let connection = Connection::connect_uri_with_identity(
+        AMQPUri::from_str(addr).ok()?,
+        ConnectionProperties::default().with_tokio(),
+        TLSConfig::default(),
+    )
+    .await
+    .ok()?;
+    let consumer = connection
+        .create_channel()
+        .await
+        .ok()?
+        .basic_consume(
+            queue,
+            consumer_tag,
+            lapin::options::BasicConsumeOptions {
+                no_ack: true, // We never have a need to ACK ourselves so let the library handle it
+                ..Default::default()
+            },
+            lapin::types::FieldTable::default(),
+        )
+        .await
+        .ok()?;
+    Some(consumer)
+}
