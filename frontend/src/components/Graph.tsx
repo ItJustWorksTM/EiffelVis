95583727ae7c50c895be405bf6d1f4c4a0d93fcc
SyncInProgress <57683502+SyncInProgress@users.noreply.github.com>
2021-09-29 19:16:43 +0200

    Minimap and info (#22)
    * Added Node Selection and Relations
    
    * Added info card for Events
    
    * Added Minimap for graph
    
    * Formatted Code Structure and Changed Minimap position

--- frontend/src/components/Graph.tsx
+++ frontend/src/components/Graph.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Graphin, { Behaviors } from '@antv/graphin'
+import { MiniMap, Tooltip } from '@antv/graphin-components'
 import dataParser from '../helpers/dataParser'
 import G6Data from '../interfaces/G6Data'
+import CustomTooltip from './CustomTooltip'
 
-const { ZoomCanvas } = Behaviors
+const { ZoomCanvas, ActivateRelations, Hoverable, ClickSelect } = Behaviors
 
 const link =
   'https://gist.githubusercontent.com/IdreesSamadi/6aa2e5f0f8c3828b41f1e3446d2002cd/raw/messages.json'
@@ -28,8 +30,23 @@ const Graph: React.FC = () => {
       fitView
       fitCenter
       theme={{ mode: 'dark' }}
-      layout={{ type: 'circular' }}
+      layout={{ type: 'dagre', rankdir: 'LR' }}
     >
+      <Tooltip bindType="node" placement="right">
+        <CustomTooltip />
+      </Tooltip>
+      <ActivateRelations trigger="click" />
+      <ClickSelect />
+      <Hoverable />
+      <MiniMap
+        visible
+        style={{
+          background: '#000000',
+          marginBottom: '3rem',
+          left: 'auto',
+          right: '0rem',
+        }}
+      />
       <ZoomCanvas />
     </Graphin>
   ) : (
