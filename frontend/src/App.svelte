5d39b8e0965ccf70f419b588b5acecb3b10269f9
AeroStun <24841307+AeroStun@users.noreply.github.com>
2022-01-09 13:33:38 +0100

    [frontend] Support @hostname backend URL prefix
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -13,6 +13,8 @@
 
 	const backend_url = process.env.EIFFELVIS_URL.startsWith('@origin')
 			          ? `${window.location.host}${window.location.pathname}${process.env.EIFFELVIS_URL.replace('@origin', '')}`
+			          : process.env.EIFFELVIS_URL.startsWith('@hostname')
+					  ? `${window.location.hostname}${process.env.EIFFELVIS_URL.replace('@hostname', '')}`
 			          : process.env.EIFFELVIS_URL;
 	const backend_has_ssl = JSON.parse(process.env.EIFFELVIS_SSL);
 	const backend_proto_ws = backend_has_ssl ? 'wss' : 'ws';
