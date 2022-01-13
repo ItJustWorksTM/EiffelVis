fd8c33818c2a31497b2419ae1ddb81730d752926
Idrees <57256007+IdreesSamadi@users.noreply.github.com>
2022-01-13 16:52:50 +0100

    fixed layout issue

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,15 +1,15 @@
 <!-- svelte-ignore a11y-missing-attribute -->
 <script lang="ts">
-  import G6 from '@antv/g6'
-  import { QueryStream, EiffelVisConnection } from './eiffelvis'
-  import { StatefulLayout } from './layout'
-  import FilterWidget from './components/FilterWidget.svelte'
-  import type { GraphSettings, Query } from './apidefinition'
-  import { deep_copy } from './utils'
-  import G6Graph from './components/G6Graph.svelte'
-  import { toggle_class } from 'svelte/internal'
+  import G6 from '@antv/g6';
+  import { QueryStream, EiffelVisConnection } from './eiffelvis';
+  import { StatefulLayout } from './layout';
+  import FilterWidget from './components/FilterWidget.svelte';
+  import type { GraphSettings, Query } from './apidefinition';
+  import { deep_copy } from './utils';
+  import G6Graph from './components/G6Graph.svelte';
+  import { toggle_class } from 'svelte/internal';
 
-  let graph_elem: G6Graph | null
+  let graph_elem: G6Graph | null;
 
   const backend_url = process.env.EIFFELVIS_URL.startsWith('@origin')
     ? `${window.location.host}${
@@ -20,27 +20,27 @@
         '@hostname',
         ''
       )}`
-    : process.env.EIFFELVIS_URL
-  const backend_has_ssl = JSON.parse(process.env.EIFFELVIS_SSL)
-  const backend_proto_ws = backend_has_ssl ? 'wss' : 'ws'
-  const backend_proto_http = backend_has_ssl ? 'https' : 'http'
+    : process.env.EIFFELVIS_URL;
+  const backend_has_ssl = JSON.parse(process.env.EIFFELVIS_SSL);
+  const backend_proto_ws = backend_has_ssl ? 'wss' : 'ws';
+  const backend_proto_http = backend_has_ssl ? 'https' : 'http';
 
   const conn = new EiffelVisConnection(
     `${backend_proto_ws}://${backend_url}/ws`
-  )
-  let stream = null
+  );
+  let stream = null;
 
-  let selected_node = null
+  let selected_node = null;
 
-  let waiting = false
+  let waiting = false;
 
-  let show_menu = false
+  let show_menu = false;
 
-  let show_legend = false
+  let show_legend = false;
 
-  let legend = new Map<string, string>()
+  let legend = new Map<string, string>();
 
-  let colors = new Array<[string, string]>()
+  let colors = new Array<[string, string]>();
 
   // TODO: make a real type
   const newDefault = () => {
@@ -65,22 +65,22 @@
         rev: false,
         pred: { type: 'SourceName', names: [] },
       },
-    }
-  }
+    };
+  };
 
-  let qhistory = []
+  let qhistory = [];
 
-  let filters = [newDefault()] as any
+  let filters = [newDefault()] as any;
 
-  let collection_modes = ['Forward', 'AsRoots']
-  let collection_mode = 'Forward'
+  let collection_modes = ['Forward', 'AsRoots'];
+  let collection_mode = 'Forward';
 
-  const range_modes = ['Time', 'Absolute', 'Ids']
-  let begin_mode = 'Absolute'
-  let begin_value = '-500'
+  const range_modes = ['Time', 'Absolute', 'Ids'];
+  let begin_mode = 'Absolute';
+  let begin_value = '-500';
 
-  let end_mode = 'Time'
-  let end_value = ''
+  let end_mode = 'Time';
+  let end_value = '';
 
   let graph_options: GraphSettings = {
     offset: 0,
@@ -89,38 +89,38 @@
     x_sep: 60,
     y_sep: 60,
     hue: 360,
-  }
+  };
 
   $: {
     if (graph_elem) {
-      graph_elem.resizeGraph()
-      selected_node = null
-      submitCurrentQuery()
+      graph_elem.resizeGraph();
+      selected_node = null;
+      submitCurrentQuery();
     }
   }
 
   const consumeQuery = async () => {
-    const layout = new StatefulLayout()
-    waiting = true
-    const iter = await stream.iter()
-    waiting = false
-    graph_elem.reset()
-    let once = true
+    const layout = new StatefulLayout();
+    waiting = true;
+    const iter = await stream.iter();
+    waiting = false;
+    graph_elem.reset();
+    let once = true;
     for await (const event of iter) {
-      layout.apply(event, graph_options)
-      graph_elem.push(event)
+      layout.apply(event, graph_options);
+      graph_elem.push(event);
 
       // TODO: Find a better way to do this
       if (once) {
-        graph_elem.focusNode(event.id)
-        once = false
+        graph_elem.focusNode(event.id);
+        once = false;
       }
-      legend = layout.getNodeColor()
+      legend = layout.getNodeColor();
     }
-  }
+  };
 
   $: {
-    colors = [...legend.entries()]
+    colors = [...legend.entries()];
   }
 
   const submitCurrentQuery = () => {
@@ -143,22 +143,22 @@
       },
       event_filters: filters
         .map((fil: any) => {
-          const ret = []
+          const ret = [];
 
           const push_if = (arr: any[], obj: any) => {
-            if (arr.length > 0) ret.push(deep_copy(obj))
-          }
+            if (arr.length > 0) ret.push(deep_copy(obj));
+          };
 
-          push_if(fil.ids.pred.ids, fil.ids)
-          push_if(fil.tags.pred.tags, fil.tags)
-          push_if(fil.types.pred.names, fil.types)
-          push_if(fil.sourcehosts.pred.hosts, fil.sourcehosts)
-          push_if(fil.sourcenames.pred.names, fil.sourcenames)
-          return ret as any
+          push_if(fil.ids.pred.ids, fil.ids);
+          push_if(fil.tags.pred.tags, fil.tags);
+          push_if(fil.types.pred.names, fil.types);
+          push_if(fil.sourcehosts.pred.hosts, fil.sourcehosts);
+          push_if(fil.sourcenames.pred.names, fil.sourcenames);
+          return ret as any;
         })
         .filter((fil: any[]) => fil.length > 0),
       collection: { type: collection_mode as 'Forward' | 'AsRoots' },
-    }
+    };
 
     qhistory = [
       ...qhistory,
@@ -170,22 +170,22 @@
         begin_value: `${begin_value}`,
         end_value: `${end_value}`,
       },
-    ]
+    ];
 
-    const newq = new QueryStream(conn, query)
-    stream = newq
-    consumeQuery()
-  }
+    const newq = new QueryStream(conn, query);
+    stream = newq;
+    consumeQuery();
+  };
 
   const onNodeSelected = async (e: any) => {
     if (e.detail?.target) {
       selected_node = await fetch(
         `${backend_proto_http}://${backend_url}/get_event/${e.detail.target._cfg.model.id}`
-      ).then((resp) => resp.json())
+      ).then(resp => resp.json());
     } else {
-      selected_node = null
+      selected_node = null;
     }
-  }
+  };
 
   const resetGraphOptions = () => {
     graph_options = {
@@ -195,23 +195,23 @@
       x_sep: 60,
       y_sep: 60,
       hue: 360,
-    }
-    consumeQuery()
-  }
+    };
+    consumeQuery();
+  };
 
   const toggleMenu = () => {
     if (show_legend) {
-      toggleLegend()
+      toggleLegend();
     }
-    show_menu = !show_menu
-  }
+    show_menu = !show_menu;
+  };
 
   const toggleLegend = () => {
     if (show_menu) {
-      toggleMenu()
+      toggleMenu();
     }
-    show_legend = !show_legend
-  }
+    show_legend = !show_legend;
+  };
 
   const options = {
     width: 400,
@@ -239,162 +239,167 @@
         },
       ],
     },
-  }
+  };
 </script>
 
 <main class="m-0 h-screen bg-base-300">
   <div
-    class="p-3 shadow-lg bg-base-100 rounded-box h-fit right-0 bottom-0 fixed w-fit m-6"
-    class:hidden={!show_menu}
+    class="flex h-fit right-0 bottom-0 fixed align-bottom justify-center items-end"
   >
-    <h1 class="text-lg py-2">Graph Options</h1>
-    <label class="input-group input-group-sm mt-1">
-      <span class="border border-[rgba(255,255,255,0.2)] span w-1/2 bg-base-100"
-        >Offset</span
-      >
-      <input
-        type="number"
-        bind:value={graph_options.offset}
-        class="input input-bordered input-sm w-1/2"
-      />
-    </label>
-    <label class="input-group input-group-sm mt-1">
-      <span class="span border border-[rgba(255,255,255,0.2)] w-1/2 bg-base-100"
-        >Time Collapse</span
-      >
-      <input
-        type="number"
-        bind:value={graph_options.time_diff}
-        class="input input-bordered input-sm w-1/2"
-      />
-    </label>
-    <label class="input-group input-group-sm mt-1">
-      <span class="span border border-[rgba(255,255,255,0.2)] w-1/2 bg-base-100"
-        >Y-axis Scaling</span
-      >
-      <input
-        type="number"
-        bind:value={graph_options.y_scale}
-        class="input input-bordered input-sm w-1/2"
-      />
-    </label>
-    <label class="input-group input-group-sm mt-1">
-      <span class="span border border-[rgba(255,255,255,0.2)] w-1/2 bg-base-100"
-        >X-axis Node Separation</span
-      >
-      <input
-        type="number"
-        bind:value={graph_options.x_sep}
-        class="input input-bordered input-sm w-1/2"
-      />
-    </label>
-    <label class="input-group input-group-sm mt-1">
-      <span class="span border border-[rgba(255,255,255,0.2)] w-1/2 bg-base-100"
-        >Y-axis Node Separation</span
-      >
-      <input
-        type="number"
-        bind:value={graph_options.y_sep}
-        class="input input-bordered input-sm w-1/2"
-      />
-    </label>
-    <label class="input-group input-group-sm mt-1">
-      <span class="span border border-[rgba(255,255,255,0.2)] w-1/2 bg-base-100"
-        >Hue (Node Color)</span
-      >
-      <input
-        type="number"
-        bind:value={graph_options.hue}
-        class="input input-bordered input-sm w-1/2"
-      />
-    </label>
-    <div class="btn-group w-full flex flex-row mt-2">
-      <button class="btn btn-sm grow btn-primary" on:click={consumeQuery}
-        >Update Graph</button
-      >
-      <button
-        class="btn btn-sm grow btn-primary btn-outline"
-        on:click={resetGraphOptions}>Reset Default</button
-      >
-    </div>
-  </div>
-
-  <div
-    class="right-0 bottom-0 m-6 inline-block absolute"
-    class:move={show_legend || show_menu}
-  >
-    <ul class="menu w-16 py-3 shadow-lg bg-base-100 rounded-box">
-      <li>
-        <a class="" class:btn-active={show_menu} on:click={toggleMenu}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="inline-block w-6 h-6 stroke-current"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 9.5H3M21 4.5H3M21 14.5H3M21 19.5H3"
-            />
-          </svg>
-        </a>
-      </li>
-      <li>
-        <a class="" class:btn-active={show_legend} on:click={toggleLegend}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="inline-block w-6 h-6 stroke-current"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-            />
-          </svg>
-        </a>
-      </li>
-    </ul>
-  </div>
-
-  <div
-    class="overflow-x-auto overflow-y-auto bg-base-100 w-0 h-fit shadow-lg absolute rounded-box right-0 bottom-0 m-6"
-    class:show={show_legend}
-  >
-    <table class="table w-full">
-      <thead>
-        <tr>
-          <th>Event Type</th>
-          <th>Color</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each colors as [event, color]}
-          <tr class="">
-            <td>{event}</td>
-            <td
-              >&nbsp
-              <div class="avatar h-3">
-                <div
-                  class="mb-8 rounded-full w-5 h-5"
-                  style="background-color: {color}"
-                />
-              </div></td
+    <div class="block m-6">
+      <ul class="menu w-16 py-3 shadow-lg bg-base-100 rounded-box">
+        <li>
+          <a class="" class:btn-active={show_menu} on:click={toggleMenu}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-6 h-6 stroke-current"
             >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 9.5H3M21 4.5H3M21 14.5H3M21 19.5H3"
+              />
+            </svg>
+          </a>
+        </li>
+        <li>
+          <a class="" class:btn-active={show_legend} on:click={toggleLegend}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-6 h-6 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+              />
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div
+      class="p-3 shadow-lg bg-base-100 rounded-box h-fit w-fit mb-6"
+      class:hidden={!show_menu}
+    >
+      <h1 class="text-lg py-2">Graph Options</h1>
+      <label class="input-group input-group-sm mt-1">
+        <span
+          class="border border-[rgba(255,255,255,0.2)] span w-1/2 bg-base-100"
+          >Offset</span
+        >
+        <input
+          type="number"
+          bind:value={graph_options.offset}
+          class="input input-bordered input-sm w-1/2 h-max"
+        />
+      </label>
+      <label class="input-group input-group-sm mt-1">
+        <span
+          class="span border border-[rgba(255,255,255,0.2)] w-1/2 bg-base-100"
+          >Time Collapse</span
+        >
+        <input
+          type="number"
+          bind:value={graph_options.time_diff}
+          class="input input-bordered input-sm w-1/2 h-max"
+        />
+      </label>
+      <label class="input-group input-group-sm mt-1">
+        <span
+          class="span border border-[rgba(255,255,255,0.2)] w-1/2 bg-base-100"
+          >Y-axis Scaling</span
+        >
+        <input
+          type="number"
+          bind:value={graph_options.y_scale}
+          class="input input-bordered input-sm w-1/2 h-max"
+        />
+      </label>
+      <label class="input-group input-group-sm mt-1">
+        <span
+          class="span border border-[rgba(255,255,255,0.2)] w-1/2 bg-base-100"
+          >X-axis Node Separation</span
+        >
+        <input
+          type="number"
+          bind:value={graph_options.x_sep}
+          class="input input-bordered input-sm w-1/2 h-max"
+        />
+      </label>
+      <label class="input-group input-group-sm mt-1">
+        <span
+          class="span border border-[rgba(255,255,255,0.2)] w-1/2 bg-base-100"
+          >Y-axis Node Separation</span
+        >
+        <input
+          type="number"
+          bind:value={graph_options.y_sep}
+          class="input input-bordered input-sm w-1/2 h-max"
+        />
+      </label>
+      <label class="input-group input-group-sm mt-1">
+        <span
+          class="span border border-[rgba(255,255,255,0.2)] w-1/2 bg-base-100"
+          >Hue (Node Color)</span
+        >
+        <input
+          type="number"
+          bind:value={graph_options.hue}
+          class="input input-bordered input-sm w-1/2 h-max"
+        />
+      </label>
+      <div class="btn-group w-full flex flex-row mt-2">
+        <button class="btn btn-sm grow btn-primary" on:click={consumeQuery}
+          >Update Graph</button
+        >
+        <button
+          class="btn btn-sm grow btn-primary btn-outline"
+          on:click={resetGraphOptions}>Reset Default</button
+        >
+      </div>
+    </div>
+    <div
+      class="overflow-x-auto overflow-y-auto bg-base-100 w-0 h-fit shadow-lg rounded-box mb-6"
+      class:show={show_legend}
+    >
+      <table class="table w-full">
+        <thead>
+          <tr>
+            <th>Event Type</th>
+            <th>Color</th>
           </tr>
-        {/each}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {#each colors as [event, color]}
+            <tr class="">
+              <td>{event}</td>
+              <td
+                >&nbsp
+                <div class="avatar h-3">
+                  <div
+                    class="mb-8 rounded-full w-5 h-5"
+                    style="background-color: {color}"
+                  />
+                </div></td
+              >
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
   </div>
 
   <div
@@ -418,13 +423,13 @@
           <button
             class="btn btn-xs btn-accent font-mono w-full select-all"
             on:click={async () => {
-              const nw = newDefault()
-              nw.ids.pred.ids = [selected_node.meta.id]
-              collection_mode = 'AsRoots'
-              filters = [nw]
-              begin_value = ''
-              end_value = ''
-              submitCurrentQuery()
+              const nw = newDefault();
+              nw.ids.pred.ids = [selected_node.meta.id];
+              collection_mode = 'AsRoots';
+              filters = [nw];
+              begin_value = '';
+              end_value = '';
+              submitCurrentQuery();
             }}>{selected_node?.meta.id}</button
           >
         </div>
@@ -506,17 +511,17 @@
           class="btn btn-sm btn-primary btn-outline basis-1/3"
           class:btn-disabled={qhistory.length <= 1 || waiting}
           on:click={() => {
-            qhistory.pop()
-            qhistory = [...qhistory]
-            const q = qhistory.pop()
-            collection_mode = q.collection_mode
-            filters = q.filters
-            begin_mode = q.begin_mode
-            begin_value = q.begin_value
-            end_mode = q.end_mode
-            end_value = q.end_value
+            qhistory.pop();
+            qhistory = [...qhistory];
+            const q = qhistory.pop();
+            collection_mode = q.collection_mode;
+            filters = q.filters;
+            begin_mode = q.begin_mode;
+            begin_value = q.begin_value;
+            end_mode = q.end_mode;
+            end_value = q.end_value;
 
-            submitCurrentQuery()
+            submitCurrentQuery();
           }}
           >{qhistory.length - 1 > 0
             ? 'undo ' + (qhistory.length - 1)
@@ -533,7 +538,7 @@
   </div>
 
   <G6Graph
-    on:nodeselected={(e) => onNodeSelected(e)}
+    on:nodeselected={e => onNodeSelected(e)}
     bind:this={graph_elem}
     {options}
     data={{}}
@@ -549,5 +554,15 @@
   }
   .move {
     margin-right: 350px;
+  }
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type='number'] {
+    -moz-appearance: textfield;
   }
 </style>
