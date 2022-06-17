0251c86591a21aad9a7e2991944f78773d4f165a
RuthgerD <RuthgerD@users.noreply.github.com>
2022-06-17 09:00:33 +0000

    deploy: f8fd31a77af249366ea7dd00540f928fb2690dd7

---
+++ docs/implementors/clap/derive/trait.Args.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["eiffelvis"] = [{"text":"impl Args for <a class=\"struct\" href=\"eiffelvis/struct.Cli.html\" title=\"struct eiffelvis::Cli\">Cli</a>","synthetic":false,"types":["eiffelvis::Cli"]}];
+implementors["event_sender"] = [{"text":"impl Args for <a class=\"struct\" href=\"event_sender/struct.Cli.html\" title=\"struct event_sender::Cli\">Cli</a>","synthetic":false,"types":["event_sender::Cli"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
