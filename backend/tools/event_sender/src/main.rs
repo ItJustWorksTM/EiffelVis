69fc415fc7c705d457356aeb3186dd70d465061a
Alexandre Rancati-Palmer <71485862+alrapal@users.noreply.github.com>
2022-11-16 17:19:02 +0100

    backend/event_sender: add eiffelvis vocabulary and improve sending options (#121)

--- backend/tools/event_sender/src/main.rs
+++ backend/tools/event_sender/src/main.rs
@@ -2,12 +2,11 @@ use std::{
     fs::File,
     io::{BufRead, BufReader},
     time::Duration,
+    time::Instant,
 };
 
-use eiffelvis_gen::{
-    event_set::{Event, EventSet, Link},
-    generator::EventGenerator,
-};
+use eiffelvis_gen::{eiffel_vocabulary::EiffelVocabulary, generator::EventGenerator};
+
 use lapin::{options::*, BasicProperties, Connection, ConnectionProperties};
 
 use clap::Parser;
@@ -16,9 +15,16 @@ use rand::{thread_rng, Rng};
 #[derive(Parser)]
 #[clap(about = "Generates random events and sends them over ampq")]
 struct Cli {
+    // NOTE: The number of events along with the total duration, min lacency and latency max is to be used for stress testing.
+    // When setting these values, keep in mind that the generator process will stop at whichever value is reached first (between the count and total duration).
+    // The defaut settings on these variables represent roughly 30,000 events sent per hour.
     /// Total amount of events to be sent (note: multiplied with the `burst` option)
-    #[clap(default_value = "1", short, long)]
+    #[clap(default_value = "30000", short, long)]
     count: usize,
+
+    /// Total duration to run the event generator
+    #[clap(default_value = "3600000", short, long)]
+    total_duration: usize,
 
     /// URL to amqp server
     #[clap(default_value = "amqp://127.0.0.1:5672/%2f", short, long)]
@@ -36,14 +42,19 @@ struct Cli {
     #[clap(long)]
     seed: Option<usize>,
 
-    /// Time in milliseconds to sleep before emitting a new burst of events
-    #[clap(default_value = "0", short, long)]
-    latency: usize,
+    /// Starting latency value for the random interval between events
+    #[clap(default_value = "5", short, long)]
+    min_latency: usize,
+
+    /// Ending latency value for the random interval between events
+    #[clap(default_value = "220", short, long)]
+    latency_max: usize,
 
     /// Amount of events to send before introducing another delay (defined with the latency option)
     #[clap(default_value = "1", short, long)]
     burst: usize,
 
+    /// Option to replay from a file (Input a path to a JSON file)
     #[clap(long)]
     replay: Option<String>,
 }
@@ -78,16 +89,7 @@ async fn app() -> anyhow::Result<()> {
         cli.seed.unwrap_or_else(|| thread_rng().gen::<usize>()),
         4,
         8,
-        EventSet::build()
-            .add_link(Link::new("Link0", true))
-            .add_link(Link::new("Link1", true))
-            .add_event(
-                Event::new("Event", "1.0.0")
-                    .with_link("Link0")
-                    .with_link("Link1"),
-            )
-            .build()
-            .expect("This should work"),
+        EiffelVocabulary.into(),
     );
 
     let mut iter: Box<dyn Iterator<Item = Vec<u8>>> = if let Some(replay_path) = cli.replay.as_ref()
@@ -101,41 +103,66 @@ async fn app() -> anyhow::Result<()> {
         Box::new(gen.iter())
     };
 
-    let target = cli.count * cli.burst;
-    let sleep_duration = Duration::from_millis(cli.latency as u64);
+    let target = cli.count;
 
     println!(
-        "Sending out ~{} events, {} events every {}ms interval",
-        target, cli.burst, cli.latency
+        "\nSending out a maximum of {} events, over a maximum duration of {} seconds. \nProcess will stop at whichever comes first. \nEvents sent at random intervals between {}-{}ms. \n",
+        target * cli.burst,
+        (cli.total_duration / 1000), cli.min_latency,
+        cli.latency_max
     );
 
     let mut sent = 0;
-    for _ in 0..(target) {
-        let mut taken = 0;
-        for ev in (&mut iter).take(cli.burst) {
-            let _ = channel_a
-                .basic_publish(
-                    cli.exchange.as_str(),
-                    cli.routing_key.as_str(),
-                    BasicPublishOptions::default(),
-                    ev.as_slice(),
-                    BasicProperties::default(),
-                )
-                .await?
-                .await?;
-            taken += 1;
-        }
-        if cli.burst > taken {
-            println!("Exhausted source, stopping early!");
-            break;
-        }
-        sent += taken;
-        if sleep_duration.as_millis() > 0 {
-            tokio::time::sleep(sleep_duration).await;
-        }
-    }
+    // Used for a time reference staring point
+    let start = Instant::now();
+    // Decalred as mut in order to allow the value to change
+    let mut run_duration = start.elapsed();
 
-    println!("Done, sent {} events", sent);
+    let t_duration = cli.total_duration.try_into().unwrap();
+
+    // Condition check to ensure min-latency is not greater than latency-max
+    if cli.min_latency <= cli.latency_max {
+        for _ in 0..(target) {
+            // Additional duration condition to break out of the loop if total duration has been reached
+            if run_duration.as_millis() < t_duration {
+                // Generate a random delay between 5 and 220ms
+                let pause_duration = Duration::from_millis(
+                    rand::thread_rng().gen_range(cli.min_latency..=cli.latency_max) as u64,
+                );
+                let mut taken = 0;
+                for ev in (&mut iter).take(cli.burst) {
+                    let _ = channel_a
+                        .basic_publish(
+                            cli.exchange.as_str(),
+                            cli.routing_key.as_str(),
+                            BasicPublishOptions::default(),
+                            ev.as_slice(),
+                            BasicProperties::default(),
+                        )
+                        .await?
+                        .await?;
+                    taken += 1;
+                }
+                if cli.burst > taken {
+                    println!("Exhausted source, stopping early!");
+                    break;
+                }
+                sent += taken;
+                // Sleep for a randomly selected time
+                tokio::time::sleep(pause_duration).await;
+                // Stores the amount of time elapsed from the start.
+                run_duration = start.elapsed();
+            } else {
+                break;
+            }
+        }
+    } else {
+        println!("Stopping early! - min-latency can not be greater than latency-max.\nTo have a fixed latency enter both as the same value\n");
+    }
+    println!(
+        "Done! Total events sent: {}, total duration: {:?}",
+        sent, run_duration
+    );
 
     Ok(())
 }
