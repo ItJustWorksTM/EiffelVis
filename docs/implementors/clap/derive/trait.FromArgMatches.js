01a4baf5a951a331cd70f363bcc879969f3f4507
RuthgerD <RuthgerD@users.noreply.github.com>
2022-11-16 11:19:56 +0000

    deploy: 73d137a939086d9816191439371b900574d80303

--- docs/implementors/clap/derive/trait.FromArgMatches.js
+++ docs/implementors/clap/derive/trait.FromArgMatches.js
@@ -1,4 +1,4 @@
-(function() {var implementors = {};
-implementors["eiffelvis"] = [{"text":"impl FromArgMatches for <a class=\"struct\" href=\"eiffelvis/struct.Cli.html\" title=\"struct eiffelvis::Cli\">Cli</a>","synthetic":false,"types":["eiffelvis::Cli"]}];
-implementors["event_sender"] = [{"text":"impl FromArgMatches for <a class=\"struct\" href=\"event_sender/struct.Cli.html\" title=\"struct event_sender::Cli\">Cli</a>","synthetic":false,"types":["event_sender::Cli"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"eiffelvis":[["impl FromArgMatches for <a class=\"struct\" href=\"eiffelvis/struct.Cli.html\" title=\"struct eiffelvis::Cli\">Cli</a>"]],
+"event_sender":[["impl FromArgMatches for <a class=\"struct\" href=\"event_sender/struct.Cli.html\" title=\"struct event_sender::Cli\">Cli</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- docs/implementors/eiffelvis_http/trait.EiffelVisHttpApp.js
+++ docs/implementors/eiffelvis_http/trait.EiffelVisHttpApp.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["eiffelvis_http"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"eiffelvis_http":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
