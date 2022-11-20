bfe06c7fca8699fb5ba990fa25e3cd8bdb6bf7a1
Ruthger Dijt <ruthgerdijt@gmail.com>
2022-11-20 19:55:05 +0100

    backend: allow client to somewhat control string matching
    adds lower_case and partial options for now,
    this means the backend will alter the way the nodes are matched.
    
    note: client provided string is not modified.

--- frontend/src/components/LineInputList.svelte
+++ frontend/src/components/LineInputList.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+
     export let values;
-    export let placeholder;
 </script>
 
 <div class="form-control">
@@ -27,12 +27,7 @@
                     />
                 </svg>
             </button>
-            <input
-                type="text"
-                {placeholder}
-                bind:value
-                class="input input-sm input-bordered basis-full"
-            />
+            <slot index={i}></slot>
         </div>
     {/each}
 </div>
