9cd712a9723e2605e6842113d2ea927e148c6c2c
Alexandre Rancati-Palmer <a.rancatipalmer@mailo.com>
2022-11-22 16:47:09 +0100

    Restored legend button lost on merge done with @hry625

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -234,6 +234,29 @@
           </a>
         </li>
         <li>
+          <a class="" class:btn-active={show_legend} on:click={toggleLegend}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-6 h-6 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+              />
+            </svg>
+          </a>
+        </li>
+        <li>
           <a
             class=""
             class:btn-active={show_timebar}
