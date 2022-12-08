85e674f8c57f2858afe662bf5657f3c65190b25a
flyf <hry87625@gmail.com>
2022-12-08 02:09:17 +0100

    frontend/FilterWidget.svelte: implement add_filter_to_TempFilterArray and add_tempFIlter_to_query

--- frontend/src/components/FilterWidget.svelte
+++ frontend/src/components/FilterWidget.svelte
@@ -1,20 +1,18 @@
 <!-- svelte-ignore a11y-missing-attribute -->
 <script lang="ts">
-    import {
+    import type {
         EventFilter,
         Id,
         SourceHost,
         SourceName,
-        string_compare_default,
         Tag,
         Type,
     } from "../apidefinition";
+    import { FilterInput, FilterType, TemperateFilterArray } from "../uitypes";
 
     import LineInputList from "./LineInputList.svelte";
-    import Input from "./TextInput.svelte";
 
-    const filter_types: string[] = ["Id", "Type", "Source", "Host", "Tag"];
-    let active_filter: string = "Id";
+    const filter_types: string[] = ["ID", "Type", "Source", "Host", "Tag"];
 
     // TODO: Maybe just fixed filter?
     export let ids: EventFilter<Id>;
@@ -22,23 +20,80 @@
     export let types: EventFilter<Type>;
     export let sourcehosts: EventFilter<SourceHost>;
     export let sourcenames: EventFilter<SourceName>;
-    let filterFields = ["Id", "Type", "Source", "Host", "Tag"];
-    let selected;
-    let inputValue;
-    let tag = tags;
-    let typess = types;
-    let sourcehostss = sourcehosts;
-    let sourcenamess = sourcenames;
+    let selected = filter_types[0];
+    let inputValue = "";
+    let tempFilterArray: TemperateFilterArray;
+    const add_filter_to_TempFilterArray = () => {
+        let newFilter = <FilterInput>{
+            active: true,
+            isWildCard: true,
+            exclude: false,
+            filterField: selected,
+            value: inputValue,
+        };
+        tempFilterArray = [...tempFilterArray, newFilter];
+        add_tempfilter_to_query();
+        inputValue = "";
+    };
+
+    export const add_tempfilter_to_query = () => {
+        tempFilterArray.forEach((filter) => {
+            if (filter.active) {
+                switch (filter.filterField) {
+                    case FilterType.ID:
+                        ids.pred.ids = [...ids.pred.ids, filter.value];
+                        break;
+                    case FilterType.Type:
+                        types.pred.names = [
+                            ...types.pred.names,
+                            {
+                                lower_case: filter.isWildCard,
+                                partial: filter.isWildCard,
+                                value: filter.value,
+                            },
+                        ];
+                        break;
+                    case FilterType.Source:
+                        sourcenames.pred.names = [
+                            ...sourcenames.pred.names,
+                            {
+                                lower_case: filter.isWildCard,
+                                partial: filter.isWildCard,
+                                value: filter.value,
+                            },
+                        ];
+                        break;
+                    case FilterType.Host:
+                        sourcehosts.pred.hosts = [
+                            ...sourcehosts.pred.hosts,
+                            {
+                                lower_case: filter.isWildCard,
+                                partial: filter.isWildCard,
+                                value: filter.value,
+                            },
+                        ];
+                        break;
+                    case FilterType.Tag:
+                        tags.pred.tags = [
+                            ...tags.pred.tags,
+                            {
+                                lower_case: filter.isWildCard,
+                                partial: filter.isWildCard,
+                                value: filter.value,
+                            },
+                        ];
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    };
 </script>
 
 <div class="w-full h-full">
     <div>
-        <LineInputList
-            placeholder={selected}
-            bind:values={ids.pred.ids}
-            active="True"
-            exclude="True"
-        />
+        <LineInputList bind:values={tempFilterArray} />
         <select class="select select-primary max-w-xs" bind:value={selected}>
             {#each filter_types as type}
                 <option>{type}</option>
@@ -47,133 +102,15 @@
         <input
             type="text"
             placeholder={selected}
+            bind:value={inputValue}
             class="input input-sm input-bordered basis-full"
         />
         <button
             class="btn btn-xs"
-            on:click={() => (ids.pred.ids = [...ids.pred.ids, ""])}>+</button
+            on:click={() => add_filter_to_TempFilterArray()}
+            disabled={inputValue == ""}>+</button
         >
     </div>
-    <!-- 
-    <div class="tabs tabs-boxed">
-        {#each filter_types as type}
-            <a
-                class="tab"
-                class:tab-active={active_filter == type}
-                on:click={() => (active_filter = type)}>{type}</a
-            >
-        {/each}
-    </div>
-    <div class:hidden={active_filter != "Id"}>
-        <label
-            class="cursor-pointer label"
-            class:hidden={ids.pred.ids.length == 0}
-        >
-            <span class="label-text">Reversed</span>
-            <input
-                type="checkbox"
-                class="toggle toggle-primary"
-                bind:checked={ids.rev}
-            />
-        </label>
-        <LineInputList bind:values={ids.pred.ids} let:index={i}>
-            <Input placeholder={"uuid"} bind:value={ids.pred.ids[i]}/>
-        </LineInputList>
-        <button
-            class="btn btn-xs w-full"
-            on:click={() => (ids.pred.ids = [...ids.pred.ids, ""])}>+</button
-        >
-    </div>
-
-    <div class:hidden={active_filter != "Type"}>
-        <label
-            class="cursor-pointer label"
-            class:hidden={types.pred.names.length == 0}
-        >
-            <span class="label-text">Reversed</span>
-            <input
-                type="checkbox"
-                class="toggle toggle-primary"
-                bind:checked={types.rev}
-            />
-        </label>
-        <LineInputList bind:values={types.pred.names} let:index={i}>
-            <Input placeholder={"type name"} bind:value={types.pred.names[i].value} />
-        </LineInputList>
-        <button
-            class="btn btn-xs w-full"
-            on:click={() => (types.pred.names = [...types.pred.names, string_compare_default()])}
-            >+</button
-        >
-    </div>
-
-    <div class:hidden={active_filter != "Source"}>
-        <label
-            class="cursor-pointer label"
-            class:hidden={sourcenames.pred.names.length == 0}
-        >
-            <span class="label-text">Reversed</span>
-            <input
-                type="checkbox"
-                class="toggle toggle-primary"
-                bind:checked={sourcenames.rev}
-            />
-        </label>
-        <LineInputList bind:values={sourcenames.pred.names} let:index={i}>
-            <Input placeholder={"source name"} bind:value={sourcenames.pred.names[i].value} />
-        </LineInputList>
-        <button
-            class="btn btn-xs w-full"
-            on:click={() =>
-                (sourcenames.pred.names = [...sourcenames.pred.names, string_compare_default()])}
-            >+</button
-        >
-    </div>
-
-    <div class:hidden={active_filter != "Host"}>
-        <label
-            class="cursor-pointer label"
-            class:hidden={sourcehosts.pred.hosts.length == 0}
-        >
-            <span class="label-text">Reversed</span>
-            <input
-                type="checkbox"
-                class="toggle toggle-primary"
-                bind:checked={sourcehosts.rev}
-            />
-        </label>
-        <LineInputList values={sourcehosts.pred.hosts} let:index={i}>
-            <Input placeholder={"host name"} bind:value={sourcehosts.pred.hosts[i].value} />
-        </LineInputList>
-        <button
-            class="btn btn-xs w-full"
-            on:click={() =>
-                (sourcehosts.pred.hosts = [...sourcehosts.pred.hosts, string_compare_default()])}
-            >+</button
-        >
-    </div>
-
-    <div class:hidden={active_filter != "Tag"}>
-        <label
-            class="cursor-pointer label"
-            class:hidden={tags.pred.tags.length == 0}
-        >
-            <span class="label-text">Reversed</span>
-            <input
-                type="checkbox"
-                class="toggle toggle-primary"
-                bind:checked={tags.rev}
-            />
-        </label>
-        <LineInputList bind:values={tags.pred.tags} let:index={i}>
-            <Input placeholder={"tag name"} bind:value={tags.pred.tags[i].value} />
-        </LineInputList>
-        <button
-            class="btn btn-xs w-full"
-            on:click={() => (tags.pred.tags = [...tags.pred.tags, string_compare_default()])}
-            >+</button
-        >
-    </div> -->
 </div>
 
 <style lang="postcss" global>
