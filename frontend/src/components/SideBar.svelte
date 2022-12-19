92ac1e0caeb2b3406a9efde3c370eaac9d91d771
Alexandre Rancati-Palmer <a.rancatipalmer@mailo.com>
2022-12-19 17:30:02 +0100

    Frontend: Address feedback about button order and trailing colon - Added a separator svg to differenciate buttons that open a panel from related issue: #53

--- frontend/src/components/SideBar.svelte
+++ frontend/src/components/SideBar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import SvgButton from "./SvgButton.svelte";
+  import ButtonSeparator from "./ButtonSeparator.svelte";
 
    export let show_menu: boolean;
    export let show_legend: boolean;
@@ -16,19 +17,7 @@
 </script>
 <div class="flex h-screen">
     <ul class="menu grid justify-items-center content-end py-3  h-screen shadow-large bg-base-300">
-      <li> 
-        <SvgButton onClickAction={toggleInteractiveModePlaceholder} 
-                btnState= {interactiveMode} 
-                data= "M12.522,10.4l-3.559,3.562c-0.172,0.173-0.451,0.176-0.625,0c-0.173-0.173-0.173-0.451,0-0.624l3.248-3.25L8.161,6.662c-0.173-0.173-0.173-0.452,0-0.624c0.172-0.175,0.451-0.175,0.624,0l3.738,3.736C12.695,9.947,12.695,10.228,12.522,10.4 M18.406,10c0,4.644-3.764,8.406-8.406,8.406c-4.644,0-8.406-3.763-8.406-8.406S5.356,1.594,10,1.594C14.643,1.594,18.406,5.356,18.406,10M17.521,10c0-4.148-3.374-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.147,17.521,17.521,14.147,17.521,10"   
-        />
-      </li>
-      <li>
-        <SvgButton
-            onClickAction= {toggleMenuPlaceholder} 
-            btnState= {show_menu}  
-            data = "M21 9.5H3M21 4.5H3M21 14.5H3M21 19.5H3" 
-         />
-      </li>
+      <!-- Buttons interracting with panels -->
       <li>
         <SvgButton 
             onClickAction= {toggleLegendPlaceholder} 
@@ -42,6 +31,23 @@
             onClickAction= {toggleFilterPanelPlaceholder} 
             btnState= {show_filter_panel} 
             data="M3, 11 a 8,8 0 1,0 16, 0 a 8,8 0 1, 0 -16, 0  M 21 21 16.65 16.65"
+        />
+      </li>
+      <li>
+        <SvgButton
+            onClickAction= {toggleMenuPlaceholder} 
+            btnState= {show_menu}  
+            data = "M21 9.5H3M21 4.5H3M21 14.5H3M21 19.5H3" 
+         />
+      </li>
+
+      <ButtonSeparator></ButtonSeparator>
+
+      <!-- Buttons interracting with graph -->
+      <li> 
+        <SvgButton onClickAction={toggleInteractiveModePlaceholder} 
+                btnState= {interactiveMode} 
+                data= "M12.522,10.4l-3.559,3.562c-0.172,0.173-0.451,0.176-0.625,0c-0.173-0.173-0.173-0.451,0-0.624l3.248-3.25L8.161,6.662c-0.173-0.173-0.173-0.452,0-0.624c0.172-0.175,0.451-0.175,0.624,0l3.738,3.736C12.695,9.947,12.695,10.228,12.522,10.4 M18.406,10c0,4.644-3.764,8.406-8.406,8.406c-4.644,0-8.406-3.763-8.406-8.406S5.356,1.594,10,1.594C14.643,1.594,18.406,5.356,18.406,10M17.521,10c0-4.148-3.374-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.147,17.521,17.521,14.147,17.521,10"   
         />
       </li>
       <li> 
